f9894ed1493573f317853dc38339b1fdbcd67bc4
Neel Patel <neel.patel6573@gmail.com>
2020-06-10 17:36:35 -0400

    merger the offsets and pattern create

--- python/create_string.py
+++ python/create_string.py
@@ -1,31 +1,78 @@
 import sys
 
-length = int(sys.argv[1])
+def create_data():
+	uppercase = [chr(i) for i in range(65,91)]
+	lowercase = [chr(i) for i in range(97,123)]
+	numbers = [chr(i) for i in range(48,58)]
 
-uppercase = [chr(i) for i in range(65,91)]
-lowercase = [chr(i) for i in range(97,123)]
-numbers = [chr(i) for i in range(48,58)]
+	return uppercase, lowercase, numbers
 
-pattern_u = ''
+def argument_check(len):
+	if len != 3:
+		print("Usage: python pattern.py <length/query>")
+		sys.exit(0)
 
-for i in uppercase:
-	pattern_u += i
-	for j in lowercase:
-		pattern_u += j
-		for k in numbers:
-			pattern_u += k
+def create_pattern(uppercase, lowercase, numbers, length):
+	pattern_u = ''
+
+	for i in uppercase:
+		pattern_u += i
+		for j in lowercase:
+			pattern_u += j
+			for k in numbers:
+				pattern_u += k
+				if len(pattern_u) == length:
+					print(pattern_u)
+					sys.exit(0)
+				elif length - len(pattern_u) == 1:
+					pattern_u += i
+					print(pattern_u)
+					sys.exit(0)
+				elif length - len(pattern_u) == 2:
+					pattern_u += j
+					print(pattern_u)
+                    sys.exit(0)
+				else:
+					pattern_u += i+j
+
+def find_offset(uppercase, lowercase, numbers, query):
+	length = 20982
+	query = query[2:]
+	bytes_object = bytearray.fromhex(query)	
+	ascii_string = bytes_object.decode("ASCII")
+	ascii_string = ascii_string[::-1]
+
+	pattern_u = ''
+
+	for i in uppercase:
+        pattern_u += i
+        for j in lowercase:
+                pattern_u += j
+                for k in numbers:
+                        pattern_u += k
 			if len(pattern_u) == length:
-				print(pattern_u)
-				sys.exit(0)
-			elif length - len(pattern_u) == 1:
-				pattern_u += i
-				print(pattern_u)
-				sys.exit(0)
-			elif length - len(pattern_u) == 2:
-				pattern_u += j
-				print(pattern_u)
-                                sys.exit(0)
-			else:
-				pattern_u += i+j
+				pass
+                        elif length - len(pattern_u) == 1:
+                                pattern_u += i
+                        elif length - len(pattern_u) == 2:
+                                pattern_u += j
+                        else:
+                                pattern_u += i+j
+
+	result = pattern_u.find(ascii_string)
+	print("The offset required to overwrite Instruction Pointer is " + str(result))
+
+if __name__ == "__main__":
+	
+	argument_check(len(sys.argv))
+	data = create_data()
+
+	if sys.argv[1] == "length":
+		length = int(sys.argv[2])
+		create_pattern(data[0], data[1], data[2]), length)
+	elif sys.argv[1] == "query":
+		query = str(sys.argv[2])
+		find_offset(data[0], Sdata[1], data[2], query)
+
 
 
